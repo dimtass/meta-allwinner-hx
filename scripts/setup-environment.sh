4ad18a3de996b193dee40649b843253bb4fba0d9
dtassopoulos <dtassopoulos@luxoft.com>
2019-11-01 16:01:59 +0100

    First commit. Made the sceleton and pushed changes to continue from another workstation

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -29,6 +29,20 @@ else
     fi
 fi
 
+# Check the distro
+LIST_DISTROS=$(ls -1 $CWD/sources/*/conf/distro | grep -F ".conf" | sed s/\.conf//g | xargs -I {} echo -e "\t"{})
+VALID_DISTROS=$(echo -e "$LIST_DISTROS" | grep ${DISTRO} | wc -l )
+if [ "x$DISTRO" = "x" ] || [ "$VALID_DISTROS" = "0" ]; then
+	echo -e "\nThe \$DISTRO you have specified ($DISTRO) is not supported by this build setup"
+    echo -e "\n \$x$DISTRO=(x$DISTRO) and \$VALID_DISTROS=($VALID_DISTROS)"
+    echo -e "\n\nLIST:\n$LIST_DISTROS\n\n"
+    return 1
+else
+    if [ ! -e $1/conf/local.conf.sample ]; then
+        echo "Configuring for distro ${DISTRO}"
+    fi
+fi
+
 OEROOT=$(pwd)/sources/poky
 if [ -e sources/oe-core ]; then
     OEROOT=sources/oe-core
@@ -54,6 +68,8 @@ if [ ! -e conf/local.conf.sample ]; then
     # Change settings according environment
     sed -e "s,MACHINE ?=.*,MACHINE = '$MACHINE',g" \
         -i conf/local.conf
+    sed -e "s,DISTRO ?=.*,DISTRO = '$DISTRO',g" \
+        -i conf/local.conf
 
     # Append in conf the number of cpus
     cat >> conf/local.conf <<EOF
@@ -77,6 +93,8 @@ for extra_script in $extra_setup_scripts; do
 done
 
 echo -e "These are the default supported images:\n${LIST_IMAGES}"
+
+echo -e "\nThese are the supported distros:\n${LIST_DISTROS}"
 cat <<EOF
 
 You can now build your image. To build the allwinner-image then run this:
